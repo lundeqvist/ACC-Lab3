1ed6004b133fc04049df97163e544edd33a759dc
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-01 11:16:57 +0200

    gg

--- init.py
+++ init.py
@@ -1,1 +1,12 @@
-test
+
+result = parse.delay()
+print result.ready()
+
+with open('home/ubuntu/testt.txt', 'w+') as f:
+	f.write(str(result.ready()))
+
+while result.ready() == False:
+	k = 1
+
+with open('home/ubuntu/test.txt', 'w+') as f:
+	f.write(result)
--- parser.py
+++ parser.py
@@ -1,16 +1,21 @@
 import os
 import json
+from celery import Celery
 
-pronouns = {"han":0, "hon":0, "den":0, "det":0, "denna":0, "denne":0, "hen":0}
+app = Celery('tasks', backend='amqp', broker='amqp://')
 
-with open('tweets_19.txt', 'r') as f:
-	for l in f:
-		try:
-			if json.loads(l)["retweeted"] == False:
-				tweet = json.loads(l)["text"].split()
-				for p in pronouns.keys():
-					if p in tweet:
-						pronouns[p] += 1
-		except:
-			pass
-print pronouns
+@app.task
+def parse():
+	pronouns = {"han":0, "hon":0, "den":0, "det":0, "denna":0, "denne":0, "hen":0}
+
+	with open('tweets_19.txt', 'r') as f:
+		for l in f:
+			try:
+				if json.loads(l)["retweeted"] == False:
+					tweet = json.loads(l)["text"].split()
+					for p in pronouns.keys():
+						if p in tweet:
+							pronouns[p] += 1
+			except:
+				pass
+	return pronouns
