63ff524630d2bbe4329b36824c8ccff38468f00d
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-01 13:47:59 +0200

    valle

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from tasks import parse
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/pronouns', methods=['GET'])
+def parse_pronouns():
+	result = parse.delay()
+
+	while result.ready() == False:
+		k = 1
+
+	data = subprocess.check_output(result.get())
+    return data
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',debug=True)
