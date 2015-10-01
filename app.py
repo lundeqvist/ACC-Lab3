7ebbae3d5dbe30beacf491dc246d7712999db7c4
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-01 14:15:01 +0200

    mathieu

--- app.py
+++ app.py
@@ -7,12 +7,10 @@ app = Flask(__name__)
 @app.route('/pronouns', methods=['GET'])
 def parse_pronouns():
 	result = parse.delay()
-
 	while result.ready() == False:
 		k = 1
-
-	data = subprocess.check_output(result.get())
-    return data
+	#data = subprocess.check_output(result.get())
+	return jsonify(result.get()),200
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',debug=True)
