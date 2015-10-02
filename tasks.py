af0ce30d0a0a24662a1bd3a221d70df1439c1fbd
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-02 12:46:47 +0200

    yay

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 import os
 import json
+import glob
 from celery import Celery
 
 app = Celery('tasks', backend='amqp', broker='amqp://')
@@ -8,14 +9,15 @@ app = Celery('tasks', backend='amqp', broker='amqp://')
 def parse():
 	pronouns = {"han":0, "hon":0, "den":0, "det":0, "denna":0, "denne":0, "hen":0}
 
-	with open('tweets_19.txt', 'r') as f:
-		for l in f:
-			try:
-				if json.loads(l)["retweeted"] == False:
-					tweet = json.loads(l)["text"].split()
-					for p in pronouns.keys():
-						if p in tweet:
-							pronouns[p] += 1
-			except:
-				pass
+	for filename in glob.glob('tweets/*.txt'):
+		with open(filename, 'r') as f:
+			for l in f:
+				try:
+					if json.loads(l)["retweeted"] == False:
+						tweet = json.loads(l)["text"].split()
+						for p in pronouns.keys():
+							if p in tweet:
+								pronouns[p] += 1
+				except:
+					pass
 	return pronouns
