824e50000524ebc4fae145d78abf79de5c4c5524
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-03 15:04:38 +0200

    hejsan

---
+++ tasks2.py
@@ -0,0 +1,10 @@
+import os
+import json
+import glob
+from celery import Celery
+
+app = Celery('tasks', backend='amqp', broker='amqp://elias.pass@130.238.29.126/geijer')
+
+@app.task
+def c(x):
+	return str(x*x*x)
