65fde601449b3099cc35c4a81f09932e877c4e20
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-03 16:28:40 +0200

    asdfgh

--- app2.py
+++ app2.py
@@ -1,6 +1,14 @@
 import os
-from remote import c
+from remote import parse
+from celery import group
 
-def cele():
-	c.delay(5)
-	c.delay(25)
+def app():
+	baseurl = 'http://smog.uppmax.uu.se:8080/swift/v1/tweets/'
+	tweets_files = ['tweets_0.txt', 'tweets_1.txt', 'tweets_2.txt', 'tweets_3.txt', 'tweets_4.txt', 'tweets_5.txt', 'tweets_6.txt', 'tweets_7.txt']
+	parser_jobs = group(parse(baseurl + url) for t_file in tweets_files)
+	result = parser_jobs.apply_async()
+	print str(result.ready())
+	while result.ready() == False:
+		k = 1
+
+	print str(result.get())
--- remote.py
+++ remote.py
@@ -1,10 +1,21 @@
 import os
-import json
+import urllib2, json
 import glob
 from celery import Celery
 
 app = Celery('tasks', backend='amqp', broker='amqp://elias:pass@130.238.29.126:5672/geijer')
 
 @app.task
-def c(x):
-	return str(x*x*x)
+def parse(url):
+	data = urllib2.urlopen(url)
+	pronouns = {"han":0, "hon":0, "den":0, "det":0, "denna":0, "denne":0, "hen":0}
+	for line in data:
+		try:
+			if json.loads(line)["retweeted"] == False:
+				tweet = json.loads(line)["text"].split()
+				for p in pronouns.keys():
+					if p in tweet:
+						pronouns[p] += 1
+		except:
+			pass
+	return pronouns
