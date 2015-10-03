33c5a1ed6f0e4b43a7f38c7fdf20675b146d639f
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-03 11:54:41 +0200

    asdf

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from tasks import parse
 from flask import Flask, jsonify
+import matplotlib.pyplot as pyplot
 
 app = Flask(__name__)
 
@@ -9,8 +10,20 @@ def parse_pronouns():
 	result = parse.delay()
 	while result.ready() == False:
 		k = 1
-	#data = subprocess.check_output(result.get())
-	return jsonify(result.get()),200
+
+	pronouns = result.get()
+
+	x_keys = pronouns.keys()
+	y_values =  pronouns.values()
+	total_values = float(sum(y_values))
+	x = [1,2,3,4,5,6,7]
+	y = [v / total_values for v in y_values]
+	pyplot.title('Pronouns')
+	pyplot.bar(x, y, align='center', width=0.65)
+	pyplot.xticks(x, x_keys)
+	pyplot.savefig('pronouns.png')
+
+	return jsonify(pronouns),200
 
 @app.route('/', methods=['GET'])
 def test():
