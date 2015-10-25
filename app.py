c4818b99137dfa75128f7be1b24bde0a977deff9
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-25 18:45:57 +0100

    a

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def parse_pronouns1():
 
 @app.route('/pronouns2/<int:n_files>', methods=['GET'])
 def parse_pronouns2(n_files):
-	if n_files > 0 and n_files < 21:
+	if n_files > 0 and n_files < 50:
 		baseurl = 'http://smog.uppmax.uu.se:8080/swift/v1/tweets/'
 		tweets_files = ['tweets_0.txt', 'tweets_1.txt', 'tweets_2.txt', 'tweets_3.txt', 'tweets_4.txt', 
 						'tweets_5.txt', 'tweets_6.txt', 'tweets_7.txt', 'tweets_8.txt', 'tweets_9.txt', 
