d0db5810d2cfc5f4b8e32da2729b0d4785626d60
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-25 17:00:27 +0100

    Fix for analysis

--- app.py
+++ app.py
@@ -37,6 +37,11 @@ def parse_pronouns1():
 	return jsonify(pronouns),200
 
 
+@app.route('/pronouns2/', methods=['GET'])
+def parse_pronouns2_default():
+	return parse_pronouns2(20)
+
+
 @app.route('/pronouns2/<int:n_files>', methods=['GET'])
 def parse_pronouns2(n_files):
 	if n_files > 0 and n_files < 21:
@@ -46,7 +51,7 @@ def parse_pronouns2(n_files):
 						'tweets_10.txt', 'tweets_11.txt', 'tweets_12.txt', 'tweets_13.txt', 'tweets_14.txt', 
 						'tweets_15.txt', 'tweets_16.txt', 'tweets_17.txt', 'tweets_18.txt', 'tweets_19.txt']
 		
-		job = group([parse2.s(baseurl + tweets) for tweets in tweets_files[0:n_files]])
+		job = group([parse2.s(baseurl + tweets) for tweets in tweets_files[0]*n_files])
 		before = time.time()
 		result = job.apply_async()
 		elapsed_time = time.time() - before
